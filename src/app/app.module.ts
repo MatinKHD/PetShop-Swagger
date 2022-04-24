f2d389bd66f90d43f29959d7cee89364f6b6e5c3
Matin KHD <matink714@gmail.com>
2022-04-24 13:26:24 +0430

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {PetModule} from "./pet/pet.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     UserModule,
-    PetModule
+    PetModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
