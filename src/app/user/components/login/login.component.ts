753d367da19ad370080fae7d6429574604e51436
Matin KHD <matink714@gmail.com>
2022-04-30 00:28:05 +0430

    initialize login page

--- src/app/user/components/login/login.component.ts
+++ src/app/user/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm! : FormGroup;
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   }
+  loginUser(){
+    const username = this.loginForm.controls['username'].value;
+    const password = this.loginForm.controls['password'].value;
 
+    this.userService.login(username, password).subscribe( () => this.router.navigate(['/pet']))
+  }
 }
