b8b484197f3234540ea4d31cbf6ce07966b31cae
Matin KHD <matink714@gmail.com>
2022-05-01 17:56:12 +0430

    enable filtering in pet catalog

--- src/app/pet/components/pet/pet.component.ts
+++ src/app/pet/components/pet/pet.component.ts
@@ -9,15 +9,39 @@ import {PetService} from "../../services/pet.service";
 export class PetComponent implements OnInit {
   public petCatalog: any[] = [];
   FPetCatalog: any[] = [];
+  Available: any;
+  Pending: any;
+  Sold: any;
   loading = false;
+
   constructor(private service: PetService) {
   }
 
   ngOnInit(): void {
-   this.getAll();
+    this.getAllAvailable();
   }
 
-  getAll(){
+  getAllAvailable() {
+    this.loading = true
+    this.service.getAll(['available']).subscribe((p: any) => {
+      this.petCatalog = p;
+      this.FPetCatalog = p;
+    }).add(() => {
+      this.loading = false;
+    })
+  }
+
+  getAllSold() {
+    this.loading = true
+    this.service.getAll(['sold']).subscribe((p: any) => {
+      this.petCatalog = p;
+      this.FPetCatalog = p;
+    }).add(() => {
+      this.loading = false;
+    })
+  }
+
+  getAllPending() {
     this.loading = true
     this.service.getAll(['pending']).subscribe((p: any) => {
       this.petCatalog = p;
@@ -28,13 +52,24 @@ export class PetComponent implements OnInit {
   }
 
   search(value: any) {
-    return  this.FPetCatalog = !value ? this.FPetCatalog = this.petCatalog : this.petCatalog.filter((p: any)  => {
-      if(p.name === undefined){
+    return this.FPetCatalog = !value ? this.FPetCatalog = this.petCatalog : this.petCatalog.filter((p: any) => {
+      if (p.name === undefined) {
         return 0;
-      }
-      else {
-       return  p.name.toLowerCase().includes(value.toLowerCase())
+      } else {
+        return p.name.toLowerCase().includes(value.toLowerCase())
       }
     });
+  }
+
+  available() {
+    return this.Available === "available"
+  }
+
+  pending() {
+    return this.Pending === "pending"
+  }
+
+  sold() {
+    return this.Sold === "sold"
   }
 }
